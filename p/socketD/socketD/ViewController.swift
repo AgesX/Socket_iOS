4e8d5bfc4ebc77a8f4b32772d920bcffa1068f21
邓江洲 <707703386@qq.com>
2020-04-15 21:00:30 +0800

    socket swift + UI

--- p/socketD/socketD/ViewController.swift
+++ p/socketD/socketD/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var hostBtn: UIButton!
+    @IBOutlet weak var joinBtn: UIButton!
+    
+    
+    @IBOutlet weak var disconnectBtn: UIButton!
+    @IBOutlet weak var boardView: UIView!
+    
+    @IBOutlet weak var replayButton: UIButton!
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func hostGame(_ sender: UIButton) {
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func joinGame(_ sender: UIButton) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func disconnectIt(_ sender: UIButton) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func replayGame(_ sender: UIButton) {
+        
+        
+        
+        
+        
+        
+    }
+    
+    
 
 }
 
