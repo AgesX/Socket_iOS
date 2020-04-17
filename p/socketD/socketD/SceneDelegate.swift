7b58b22a8c61d81c648101d5439a633e80a82385
邓江洲 <707703386@qq.com>
2020-04-17 15:01:47 +0800

    print(dateFormatterGet.string(from: Date()))

--- p/socketD/socketD/SceneDelegate.swift
+++ p/socketD/socketD/SceneDelegate.swift
@@ -17,6 +17,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        
+        
+        
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "MMM dd,yyyy"
+/*
+        if let date = dateFormatterGet.date(from: "2016-02-29 12:24:26") {
+            print(dateFormatterPrint.string(from: date))
+        } else {
+           print("There was an error decoding the string")
+        }
+  */
+        
+        
+        print(dateFormatterGet.string(from: Date()))
+        
+        
+        
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
